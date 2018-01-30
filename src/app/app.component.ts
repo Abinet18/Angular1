c906fb922ac2d9add58c8b25d0ea6ea8cf392493
unknown <abinet.tafa@gmail.com>
2018-01-30 08:17:14 -0600

    two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   //templateUrl: './app.component.html',
   template: `<h1>Welcome to my counter app</h1>
-             <p>Input counter value(from parent) : <input type=text #counterTxt (input)="ComponentCounterValue=counterTxt.value"> </p>
+             <p>Input counter value(from parent) : <input type=text #counterTxt [value]="ComponentCounterValue" (input)="ComponentCounterValue=counterTxt.value"> </p>
              <app-counter [counterValue]="ComponentCounterValue" (counterChange)="ComponentCounterValue=$event"></app-counter>
              <p>Component Counter value in parent: {{ComponentCounterValue}}</p>`,
   styleUrls: ['./app.component.css']
